0b9d70337efdc809d3d8ed696099af1cc564277e
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-09 17:55:12 +0000

    back end connected to front end

--- src/server.js
+++ src/server.js
@@ -5,14 +5,27 @@ import authorsRouter from "./api/authors/index.js";
 import blogPostsRouter from "./api/blogPosts/index.js";
 
 import { badRequestHandler, notFoundHandler, genericHandler } from "./errorHandlers.js"
+import createHttpError from "http-errors";
 
 
 const server = express();
 const port = 3001;
 
 /*----------Middlewares----------*/
-server.use(cors());
+const whitelist = [process.env.FE_DEV_URL]
+
+server.use(cors({
+    origin: (currentOrigin, corsNext) => {
+        if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+            corsNext(null, true)
+        } else {
+            corsNext(createHttpError(400, `Origin ${currentOrigin} is not in the whitelist!`))
+        }
+    }
+}));
+
 server.use(express.json());
+
 
 /*----------Endpoints----------*/
 server.use("/authors", authorsRouter);
@@ -27,5 +40,6 @@ server.use(genericHandler);
 
 server.listen(port, () => {
     console.table(listEndpoints(server));
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`)
+    console.log(whitelist);
 });
